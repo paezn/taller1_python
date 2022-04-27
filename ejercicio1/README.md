1eeea70de8b93a19afb4e2aa32afec5b3f3c6997
paezn <npaez@colegioguanenta.edu.co>
2022-04-27 07:12:52 -0500

    Desarrollo: ejercicio 1 resuelto

---
+++ ejercicio1/README.md
@@ -0,0 +1,7 @@
+# Ejercicio 1:
+
+## Dado el valor de venta de producto, calcular su IVA y el precio final de venta.
+
+## DIAGRAMA DE FLUJO
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
--- ejercicio1/iva.py
+++ ejercicio1/iva.py
@@ -7,9 +7,14 @@ print("----IVA PRECIO DE VENTA -------")
 print("-------------------------------")
 
 # input
-
+precio = int(input("Digite el precio del producto: "))
 
 # processing
-
+iva = 0.19*precio
+precio_venta = precio + iva
 
 # output
+print("\n\nRESULTADOS:\n")
+print("Precio: " + str(precio))
+print("IVA: " +  str(iva))
+print("Precio de venta: " + str(precio_venta))
